30df9f24cb56bdb64d0264886dd42549b1258b73
Mohammad Zaid <88818297+zetapii@users.noreply.github.com>
2023-07-06 20:25:16 +0530

    Update VersionQueue.cpp

--- VersionQueue.cpp
+++ VersionQueue.cpp
@@ -1,5 +1,64 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+template<typename T>
+class Vector
+{
+    private:
+    T* vec;
+    size_t cap;
+    size_t siz;
+    public:
+    Vector()
+    {
+        vec = new T[1];
+        cap = 1;
+        siz = 0;
+    }
+    ~Vector()
+    {
+        delete [] vec;
+    }
+    int size()
+    {
+        return siz;
+    }
+    void push_back(const T element)
+    {
+        if(siz==cap)
+        {
+            T* tmp = new T[2*cap];
+            for(int i=0;i<siz;i++)
+                tmp[i]=vec[i];
+            delete [] vec;
+            cap*=2;
+            vec=tmp;
+        }
+        vec[siz++]=element;
+        return ;
+    }
+    T& back() 
+    {
+        return vec[siz - 1];
+    }
+    const T& operator[](size_t index) const
+    {
+        if (index >= siz) 
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return vec[index];
+    }
+    T& operator[](size_t index)
+    {
+        if (index >= siz) 
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return vec[index];
+    }
+};
+
 
 template<typename T>
 struct node 
@@ -18,17 +77,19 @@ struct node
 template<typename T>
 class VersionQueue
 {
-private:
-    int curVersion;
-    vector<node<T>*> head;  
-    vector<node<T>*> tail; 
+    private:
 
-public:
+    int curVersion;
+    Vector<node<T>*> head;  
+    Vector<node<T>*> tail; 
+
+    public:
+
     VersionQueue()
     {
         curVersion = 0;
-        head.resize(1, NULL);
-        tail.resize(1, NULL);
+        head.push_back(NULL);
+        tail.push_back(NULL);
     }
 
     void enqueue(T x)
@@ -42,10 +103,11 @@ public:
         else 
         {
             curVersion++;
-            head.push_back(head[curVersion - 1]);
-            tail.push_back(new node<T>(x, tail[curVersion - 1], NULL));
+            head.push_back(head[curVersion - 1]);            
+            tail.push_back(new node<T>(x, tail[curVersion - 1], NULL));          
             tail[curVersion - 1]->nxt = tail[curVersion];
         }
+
     }
 
     void dequeue()
@@ -82,13 +144,21 @@ public:
 
 int main() 
 {
-    VersionQueue<int> qu;
-    qu.enqueue(4);
-    qu.enqueue(5);
-    qu.print(1);
-    qu.dequeue();
-    qu.enqueue(100);
-    qu.enqueue(200);
-    qu.print(4);
+    try
+    {
+        /* code */
+        VersionQueue<int> qu;
+        qu.enqueue(4);
+        qu.enqueue(5);
+        qu.print(1);
+        qu.dequeue();
+        qu.enqueue(100);
+        qu.enqueue(200);
+        qu.print(4);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     return 0;
 }
